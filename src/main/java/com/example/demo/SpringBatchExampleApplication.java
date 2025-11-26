ac65a5a892711e480e0886f7ed6a63638e946a47
yamini4 <yamini4.g@gmail.com>
2025-11-26 16:19:38 +0530

    adding interview question

--- src/main/java/com/example/demo/SpringBatchExampleApplication.java
+++ src/main/java/com/example/demo/SpringBatchExampleApplication.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -20,7 +21,7 @@ public class SpringBatchExampleApplication {
 
 	public static void main(String[] args) {
 
-		// DEMO DATA
+//		// DEMO DATA
 		List<Person> list = Arrays.asList(new Person(10, "Arun"), new Person(3, "John"), new Person(7, "Priya"),
 				new Person(1, "Karan"));
 
@@ -28,7 +29,32 @@ public class SpringBatchExampleApplication {
 		sortedSet.addAll(list);
 		sortedSet.stream().forEach(System.out::println);
 
-		SpringApplication.run(SpringBatchExampleApplication.class, args);
+		// Given an array of integers, find the pair of numbers that sum up to a target
+		// value
+
+		int[] intArr = { 1, 2, 3, 4, 5, 6, 7 };
+		int target = 7;
+
+		for (int i = 0; i < intArr.length - 1; i++) {
+			for (int j = i; j < intArr.length; j++) {
+				if (intArr[i] + intArr[j] == target) {
+					System.out.println(intArr[i] + "====" + intArr[j]);
+				}
+			}
+
+		}
+
+		Map<Integer, Integer> targetMap = new HashMap<>();
+
+		for (int i = 0; i < intArr.length; i++) {
+			int compareValue = target - intArr[i];
+			if (targetMap.containsKey(compareValue)) {
+				System.out.println(compareValue + "====" + intArr[i]);
+			}
+			targetMap.put(intArr[i], i);
+		}
+
+		// SpringApplication.run(SpringBatchExampleApplication.class, args);
 	}
 
 }
