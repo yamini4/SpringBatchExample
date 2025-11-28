762c87945b556f12a8fc81e5cf54e99b772b3abd
yamini4 <yamini4.g@gmail.com>
2025-11-28 12:30:46 +0530

    adding stream solutions

--- src/main/java/com/example/demo/SpringBatchExampleApplication.java
+++ src/main/java/com/example/demo/SpringBatchExampleApplication.java
@@ -7,17 +7,17 @@ import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.example.demo.entity.Employee;
 import com.example.demo.entity.Person;
 
-@SpringBootApplication
-@EnableBatchProcessing
-@EnableScheduling
+//@SpringBootApplication
+//@EnableBatchProcessing
+//@EnableScheduling
 public class SpringBatchExampleApplication {
 
 	public static void main(String[] args) {
@@ -30,20 +30,32 @@ public class SpringBatchExampleApplication {
 		sortedSet.addAll(list);
 		sortedSet.stream().forEach(System.out::println);
 
-		///////////////////////////////////////////////////
-		List<Employee> listEmp = Arrays.asList(new Employee(10, "Arun", 3473097, "ban", 'F'),
-				new Employee(3, "John", 3473097, "htd", 'F'), new Employee(7, "Priya", 3473097, "ban", 'M'),
-				new Employee(1, "Karan", 3473097, "ban", 'M'));
+		// /////////////////////////////////////////////////
+		List<Employee> listEmp = Arrays.asList(new Employee(10, "Arun", 60000, "ban", 'F'),
+				new Employee(3, "John", 70000, "htd", 'F'), new Employee(7, "Priya", 40000, "ban", 'M'),
+				new Employee(1, "Karan", 30000, "ban", 'M'));
 
 		// ASC Sorting by Name
 		List<Employee> sortAsc = listEmp.stream().sorted(Comparator.comparing(Employee::getName))
 				.collect(Collectors.toList());
+		System.out.println("ASC Sorting by Name");
 		sortAsc.forEach(System.out::println);
 
 		// DESC Sorting by Name
 		List<Employee> sortDesc = listEmp.stream().sorted(Comparator.comparing(Employee::getName).reversed())
 				.collect(Collectors.toList());
+		System.out.println("DESC Sorting by Name");
 		sortDesc.forEach(System.out::println);
+
+		// DECS Sorting by Salary
+		List<Employee> sortDescSalary = listEmp.stream().sorted(Comparator.comparing(Employee::getSalary).reversed())
+				.collect(Collectors.toList());
+
+		Employee secondHighSalaryEmp = listEmp.stream().sorted(Comparator.comparing(Employee::getSalary).reversed())
+				.skip(1).findFirst().orElse(null);
+
+		System.out.println(sortDescSalary.get(1));
+		System.out.println("secondHighSalaryEmp" + secondHighSalaryEmp);
 
 		// Modify salary based on condition
 		List<Employee> modifiedEmp = listEmp.stream().map(e -> {
@@ -52,6 +64,7 @@ public class SpringBatchExampleApplication {
 			}
 			return e;
 		}).collect(Collectors.toList());
+		System.out.println("Modify salary based on condition");
 		modifiedEmp.forEach(System.out::println);
 
 		// Sum of salary for employees from "ban"
@@ -68,8 +81,7 @@ public class SpringBatchExampleApplication {
 		System.out.println("Female Count: " + countFemale);
 		System.out.println("Male Count: " + countMale);
 
-		SpringApplication.run(SpringBatchExampleApplication.class, args);
-
+		//SpringApplication.run(SpringBatchExampleApplication.class, args);
 	}
 
 }
